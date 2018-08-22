909e7cb452d646c1ab63d2913483f256e95de06d
SaurabDey <saurab.jeet@gmail.com>
2018-08-22 08:57:19 +0530

    Changes done for pull

--- src/test/java/com/test/LatestProject/SaurabClass.java
+++ src/test/java/com/test/LatestProject/SaurabClass.java
@@ -2,25 +2,11 @@ package com.test.LatestProject;
 
 public class SaurabClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 	
 		System.out.println("Test Autpmation");
-		System.out.println("Test Autpmation");
-		System.out.println("Test Autpmation");
-		System.out.println("Test Autpmation");
-		System.out.println("Test Autpmation");
-		System.out.println("Test Autpmation");
-		System.out.println("Test Autpmation");
-		System.out.println("Test Autpmation");System.out.println("Test Autpmation");
-		System.out.println("Test Autpmation");
-		System.out.println("Test Autpmation");
-		System.out.println("Test Autpmation");System.out.println("Test Autpmation");
-		System.out.println("Test Autpmation");
-		System.out.println("Test Autpmation");
-		System.out.println("Test Autpmation");System.out.println("Test Autpmation");
-		System.out.println("Test Autpmation");
-		System.out.println("Test Autpmation");
-		System.out.println("Test Autpmation");
+	
+		Thread.sleep(6000);
 	}
 
 }
